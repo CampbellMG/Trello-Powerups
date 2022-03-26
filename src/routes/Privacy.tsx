b8bd033c3e2ad41442f9300da56c7d29c50d07dd
CampbellMG <campbell.gorman@gmail.com>
2022-03-26 19:33:09 +1100

    Updated privacy policy

--- src/routes/Privacy.tsx
+++ src/routes/Privacy.tsx
@@ -7,18 +7,19 @@ export const Privacy = () => (
         <Title>Privacy Policy 🔒</Title>
         <p>
             All CMGCode power-ups are completely open source and solely rely on the client (your web browser) to process
-            your data. No plugins access personally identifiable information or submit data to anywhere except Trello.
+            your data. No power-up will ever access personally identifiable information or submit data anywhere except
+            Trello.
         </p>
         <p>
-            Some power-ups (such as the Merge Lists power-up) may request you to authorise the plugin on your behalf.
-            This is necessary for us to be able to access board data such as checklists or add new data such as cards.
-            We will never modify a board without your request or access board data without your knowledge. If you would
-            like to revoke a plugin's authorisation you can do so by selecting the "Remove personal settings" button in
-            the plugin settings.
+            Some power-ups (such as the Merge Lists power-up) may request you to authorise the power-up on your behalf.
+            This is necessary for the power-up to access board data such as checklists or add new data such as cards. A
+            power-up will never modify a board without your request or access board data without your knowledge. If you
+            would like to revoke a your authorisation you can do so by selecting the "Remove personal settings" button
+            in the power-up's settings.
         </p>
         <p>
             If you would like to have a look at what's going on behind the scenes you can find the source code for all
-            plugins here: <a href={Config.repo}>{Config.repo}</a>.
+            power-ups here: <a href={Config.repo}>{Config.repo}</a>.
         </p>
         <p>
             If you have any questions or concerns, please contact me at <a href={Config.email}>{Config.email}</a>.
